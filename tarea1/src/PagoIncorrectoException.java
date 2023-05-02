32b4bad5f75138edfbefecd0a91b757301a8c94f
shilu <antguajardo2021@udec.cl>
2023-05-02 09:54:21 -0400

    Javadoc para exepciones. PagoIncorrectoException PagoInsuficienteException NoHayProductoExcption

--- tarea1/src/PagoIncorrectoException.java
+++ tarea1/src/PagoIncorrectoException.java
@@ -1,4 +1,13 @@
+/** clase para la exepción en caso de que el pago del producto sea invalido
+ *
+ */
 public class PagoIncorrectoException extends NullPointerException{
+
+    /**
+     * constructor el cual recibe la exepción si es que el pago es invalido
+     *
+     * @param pagoinvalido
+     */
     public PagoIncorrectoException(String pagoinvalido){
         super(pagoinvalido);
     }
--- tarea1/src/PagoInsuficienteException.java
+++ tarea1/src/PagoInsuficienteException.java
@@ -1,4 +1,13 @@
+/** clase para la exepción en caso de que el que el valor ingresado sea inferior
+ * al solicitado
+ */
 public class PagoInsuficienteException extends Exception{
+
+    /** constructor el cual recibe la exepción si es que el valor ingresado es inferior
+     * al solicitado
+     *
+     * @param noalcanza
+     */
     public PagoInsuficienteException(String noalcanza){
         super(noalcanza);
     }
