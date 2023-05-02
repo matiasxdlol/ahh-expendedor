dfa105ef5edb83974805b190c600dca7942dafdd
shilu <antguajardo2021@udec.cl>
2023-05-01 23:43:58 -0400

    commit

---
+++ tarea1/src/Producto.java
@@ -0,0 +1,11 @@
+abstract class Producto {
+    private int serie;
+
+    public Producto(int serie){
+        this.serie=serie;
+    }
+
+    public int GetSerie(){
+        return serie;
+    }
+}
