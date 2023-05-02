b375819b2ebce087440ad0c7ad8a35cf493444eb
shilu <antguajardo2021@udec.cl>
2023-05-02 09:13:42 -0400

    javadoc de Bebida y sus subclases

--- tarea1/src/Bebida.java
+++ tarea1/src/Bebida.java
@@ -5,7 +5,7 @@ superclase abstracta para CocaCola y Sprite
 abstract class Bebida extends Producto{
 
 /**
-constructor de bebida para ver si es cocacola o sprite
+ constructor el cual recibe la serie de la bebida
  */
     public Bebida(int serie) {
         super(serie);
@@ -19,7 +19,7 @@ constructor de bebida para ver si es cocacola o sprite
  */
 class CocaCola extends Bebida {
 
-    /** constructor de cocacola
+    /** constructor el cual recibe la serie de cocacola
      *
      * @param serie
      */
@@ -30,17 +30,35 @@ class CocaCola extends Bebida {
         super(serie);
     }
 
+    /** metodo para obtener el nombre de la bebida con un getter
+     *
+     * @return cocacola
+     */
     public String getnombre() {
         return "cocacola";
     }
 }
+
+/** subclase de bebida
+ *
+ */
 class Sprite extends Bebida  {
+
+    /** constructor el cual recibe la serie de sprite
+     *
+     * @param serie
+     */
     public Sprite(int serie)
     {
         super(serie);
     }
 
     public String getnombre() {
+
+        /** metodo para obtener el nombre de la bebida con un getter
+         *
+         * @return sprite
+         */
 
         return "sprite";
     }
