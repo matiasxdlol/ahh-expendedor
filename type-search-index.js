0cded931431b76504b31279137e46d47fe8b9f67
shilu :) <131009526+shiiilu@users.noreply.github.com>
2023-05-02 10:48:22 -0400

    Delete type-search-index.js

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Moneda100"},{"p":"<Unnamed>","l":"Moneda1000"},{"p":"<Unnamed>","l":"Moneda1500"},{"p":"<Unnamed>","l":"Moneda500"}];updateSearchResults();
