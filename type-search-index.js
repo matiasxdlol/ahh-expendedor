a8ccb3db4337ec953a16c965e8914c8e15eed412
shilu <antguajardo2021@udec.cl>
2023-05-01 23:13:22 -0400

    HOLA

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Moneda100"},{"p":"<Unnamed>","l":"Moneda1000"},{"p":"<Unnamed>","l":"Moneda1500"},{"p":"<Unnamed>","l":"Moneda500"}];updateSearchResults();
